10757bf862dff8b58baa11be9f4d00acd8451d2d
brett.odgers-jewell@avanade.com <brett.odgers-jewell@avanade.com>
2023-08-08 14:39:37 +0800

    update module

--- naming-convention.tf
+++ naming-convention.tf
@@ -1,13 +1,11 @@
-#storage account container name
 module "vnet_name" {
-  source      = "git::https://github.com/BrettOJ/tf-az-module-naming-convention?ref=main"
-  name_format = "res_type|-|project_code|-|env|zone|tier|-|name"
+  source        = "git::https://dev.azure.com/innersource/Merlion/_git/tf-module-az-naming-convention?ref=main"
+  name_format   = "res_type|-|project_code|-|env|zone|tier|-|name"
+  resource_type = "vnet"
   naming_convention_info = {
-    for key, value in var.resource_groups :
-    "${key}" => {
-      name_info = merge(value.naming_convention_info, { name = value.name })
-      tags      = value.tags
+    "${var.naming_convention_info.name}" = {
+      name_info = var.naming_convention_info
+      tags = var.tags
     }
   }
-  resource_type = "vnet"
 }
